fa9a569bcc6925eca62f6a907b76613888a70b31
Storm <oguner@gmail.com>
2021-07-28 14:59:16 +0200

    Delete goto_repo_root.sh

--- goto_repo_root.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Go up to the directory where .repo exists. 
-# Similar effect with croot function defined in <android>/build/envsetup.sh
-# Ref: https://unix.stackexchange.com/a/13474
-function upsearch () {
-    test / == "$PWD" && return || test -e "$1" && echo "found: " "$PWD" && return || cd .. && upsearch "$1"
-}
-
